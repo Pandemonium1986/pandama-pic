d16ce7dbafa894d1f29ee5a747eaf48ce45eb58c
Pandemonium1986 <michael.maffait@gmail.com>
2019-06-11 23:32:54 +0200

    upd : move from ip to fqdn

--- portainer/portainer.sh
+++ portainer/portainer.sh
@@ -24,44 +24,44 @@ export PATH="$HOME/.local/bin":$PATH
 
 #-- Portainer configuration
 export PORTAINER_TOKEN=""
-PORTAINER_TOKEN=$(http POST :9000/api/auth Username="admin" Password="$PORTAINER_ADMIN_PASSWORD" | jq -r .jwt)
+PORTAINER_TOKEN=$(http POST portainer.docker.local/api/auth Username="admin" Password="$PORTAINER_ADMIN_PASSWORD" | jq -r .jwt)
 
 # Renaming endpoint
-http PUT :9000/api/endpoints/1 \
+http PUT portainer.docker.local/api/endpoints/1 \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Name="Docker Local Unix Socket"
 
 # Teams
-http POST :9000/api/teams \
+http POST portainer.docker.local/api/teams \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Name="dev"
-http POST :9000/api/teams \
+http POST portainer.docker.local/api/teams \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Name="ops"
 
 # Users
-http POST :9000/api/users \
+http POST portainer.docker.local/api/users \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Username="alice" Password="password1*" Role:=2
-http POST :9000/api/users \
+http POST portainer.docker.local/api/users \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Username="bob" Password="password1*" Role:=2
-http POST :9000/api/users \
+http POST portainer.docker.local/api/users \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Username="charlie" Password="password1*" Role:=2
 
 # Memberships
-http POST :9000/api/team_memberships \
+http POST portainer.docker.local/api/team_memberships \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   UserID:=2 TeamID:=1 Role:=1
-http POST :9000/api/team_memberships \
+http POST portainer.docker.local/api/team_memberships \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   UserID:=3 TeamID:=2 Role:=1
-http POST :9000/api/team_memberships \
+http POST portainer.docker.local/api/team_memberships \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   UserID:=4 TeamID:=1 Role:=2
 
 # Endpoint Access
-http PUT :9000/api/endpoints/1/access \
+http PUT portainer.docker.local/api/endpoints/1/access \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   AuthorizedTeams:=[1,2]
