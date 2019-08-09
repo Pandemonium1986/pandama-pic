f5307dbb8186d00871b5dde565986dcbfd120388
Pandemonium1986 <michael.maffait@gmail.com>
2019-08-09 22:51:17 +0200

    upd : shellcheck

--- portainer/portainer.sh
+++ portainer/portainer.sh
@@ -16,8 +16,8 @@ then
 fi
 
 #-- HTTPie options
-export HTTP_OPTIONS=""
-HTTP_OPTIONS="--ignore-stdin"
+export HTTPIE_OPTIONS=""
+HTTPIE_OPTIONS="--ignore-stdin"
 
 # # Fix Path on debian 9
 # export PATH="$HOME/.local/bin":$PATH
@@ -28,44 +28,44 @@ HTTP_OPTIONS="--ignore-stdin"
 ##########
 #-- Portainer configuration
 export PORTAINER_TOKEN=""
-PORTAINER_TOKEN=$(http $HTTP_OPTIONS POST portainer.docker.local/api/auth Username="admin" Password="$PORTAINER_ADMIN_PASSWORD" | jq -r .jwt)
+PORTAINER_TOKEN=$(http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/auth Username="admin" Password="$PORTAINER_ADMIN_PASSWORD" | jq -r .jwt)
 
 # Renaming endpoint
-http $HTTP_OPTIONS PUT portainer.docker.local/api/endpoints/1 \
+http "$HTTPIE_OPTIONS" PUT portainer.docker.local/api/endpoints/1 \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Name="Docker Local Unix Socket"
 
 # Teams
-http $HTTP_OPTIONS POST portainer.docker.local/api/teams \
+http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/teams \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Name="dev"
-http $HTTP_OPTIONS POST portainer.docker.local/api/teams \
+http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/teams \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Name="ops"
 
 # Users
-http $HTTP_OPTIONS POST portainer.docker.local/api/users \
+http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/users \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Username="alice" Password="password1*" Role:=2
-http $HTTP_OPTIONS POST portainer.docker.local/api/users \
+http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/users \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Username="bob" Password="password1*" Role:=2
-http $HTTP_OPTIONS POST portainer.docker.local/api/users \
+http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/users \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   Username="charlie" Password="password1*" Role:=2
 
 # Memberships
-http $HTTP_OPTIONS POST portainer.docker.local/api/team_memberships \
+http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/team_memberships \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   UserID:=2 TeamID:=1 Role:=1
-http $HTTP_OPTIONS POST portainer.docker.local/api/team_memberships \
+http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/team_memberships \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   UserID:=3 TeamID:=2 Role:=1
-http $HTTP_OPTIONS POST portainer.docker.local/api/team_memberships \
+http "$HTTPIE_OPTIONS" POST portainer.docker.local/api/team_memberships \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   UserID:=4 TeamID:=1 Role:=2
 
 # Endpoint Access
-http $HTTP_OPTIONS PUT portainer.docker.local/api/endpoints/1 \
+http "$HTTPIE_OPTIONS" PUT portainer.docker.local/api/endpoints/1 \
   "Authorization: Bearer $PORTAINER_TOKEN" \
   UserAccessPolicies:='{"2":{"RoleID":1},"3":{"RoleID":2}}'
